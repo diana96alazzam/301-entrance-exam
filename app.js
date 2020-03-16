04e03af3480abc6f0b1858de72481d02a7092934
dianaazzam <dianaazzam@DESKTOP-9F18RBI.localdomain>
2020-03-16 15:15:03 +0200

    finishedExam

--- app.js
+++ app.js
@@ -10,10 +10,33 @@ function Task(taskName, taskDate) {
 
 var tasksArray = [];
 
+
+var month = new Array();
+month[0] = "JAN";
+month[1] = "FEB";
+month[2] = "MAR";
+month[3] = "APR";
+month[4] = "MAY";
+month[5] = "JUN";
+month[6] = "JUL";
+month[7] = "AUG";
+month[8] = "SEP";
+month[9] = "OCT";
+month[10] = "NOV";
+month[11] = "DEC";
+
 var dateH = new Date();
+var dateD = dateH.getDate();
+var monthD = month[dateH.getMonth()];
+var yearD = dateH.getFullYear();
+
+
+var submitCount=0;
+
+
 
 var currentDateH = document.getElementById('currentDate');
-currentDateH.textContent = dateH;
+currentDateH.textContent = `${monthD} ${dateD} ${yearD}`;
 
 var task = document.getElementById('task');
 var inputtedTaskDate = document.getElementById('dateInput');
@@ -24,14 +47,15 @@ taskForm.addEventListener('submit', addtask);
 
 function addtask(event) {
     event.preventDefault();
+    submitCount++;
 
     var taskName = event.target.task.value;
-    var task1name = document.createElement('li');
-    task1name.textContent = `1. ${taskName}`;
+    var task1name = document.createElement('h2');
+    task1name.textContent = `${submitCount}. ${taskName}`;
     taskList.appendChild(task1name);
 
 
-    var task1Date = document.createElement('li');
+    var task1Date = document.createElement('p');
     task1Date.textContent = inputtedTaskDate.value;
     taskList.appendChild(task1Date);
 
@@ -56,11 +80,11 @@ function retrieveTask() {
         tasksArray = JSON.parse(tasksString);
 
         for (var i = 0; i < tasksArray.length; i++) {
-            var task1name = document.createElement('li');
-            task1name.textContent = `1. ${tasksArray[i].taskName}`;
+            var task1name = document.createElement('h2');
+            task1name.textContent = `${i+1}. ${tasksArray[i].taskName}`;
             taskList.appendChild(task1name);
 
-            var task1Date = document.createElement('li');
+            var task1Date = document.createElement('p');
             task1Date.textContent = tasksArray[i].taskDate;
             taskList.appendChild(task1Date);
 
